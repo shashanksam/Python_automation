909c01c5fd1803ad9001f7d00bc1ed12cbb4db43
ShashankSamS <35063506+shashanksam@users.noreply.github.com>
2022-11-23 13:43:16 +0530

    Updated code to extract fields data
    Updated code to extract fields data from json format and entity dto format and compare the fields case sensitivity.

--- Shortcuts/Compare_fields_case_sensitive.py
+++ Shortcuts/Compare_fields_case_sensitive.py
@@ -1,48 +1,76 @@
+import re
+
+
+
+def filterFieldsFromJSON(ListOfJsons):
+    FieldsFromJsonList=[]
+    for jsonfield in ListOfJsons:
+        field=re.search("\"(.*)\":", jsonfield)
+        if(field):
+            FieldsFromJsonList.append(field.group(1))
+#             print(field.group(1))
+    return FieldsFromJsonList
+
+
+def filterFieldsFromJavaEntityFile(ListOfentity):
+    FieldsFromEntityList=[]
+    for entityfield in ListOfentity:
+        field=re.search("private String(.*);", entityfield)
+        if(field):
+            FieldsFromEntityList.append(field.group(1).strip())
+#             print(field.group(1).strip())
+    return FieldsFromEntityList
+
 
 
 def popOutDifferentFields():
-    payloadListCollection="""approved_By			
-approved_Flag		
-approver_COMMENTS	
-cLient				
-current_Status		
-customer_No			
-job_Run_Date		
-language_Key		
-market				
-modified_By			
-modified_Date		
-modified_Time		
-name				
-placeHolder			
-placeholderextra7	
-release				
-text				
-text_ID
+    payloadListCollection=""""placeholder": "90",
+"material_No": "",
+"length": null,
+"width": null,
+"height": null,
+"approved_Flag": "AWAITING",
+"modified_Date": "21",
+"modified_By": "dated",
+"approved_By": null,
+"current_status": null,
+"error_DETAILS": null,
+"modified_Time": "13:31:3",
+"load_PROCESS": "CREATE",
+"approver_COMMENTS": null,
+"placeholderextra7": ""
+
     """
 
-    entityListCollection="""approved_By
-approved_Flag
-approver_COMMENTS
-client
-current_Status
-customer_No
-job_Run_Date
-language_Key
-market
-modified_By
-modified_Date
-modified_Time
-name
-placeholder
-PLACEHOLDEREXTRA7
-release
-text
-text_Id
-    """
+    entityListCollection="""
+private String	placeholder	;
+private String	material_No	;
+private String	length	;
+private String	width	;
+private String	height	;
+
+private String	approved_Flag	;
+private String	modified_Date	;
+private String	modified_By	;
+private String	approved_By	;
+private String	current_status;
+private String	error_DETAILS;
+private String	modified_Time;
+private String	load_PROCESS;
+private String  approver_COMMENTS;
+
+private String placeholderextra7;"""
 
     payloadListOld=payloadListCollection.split("\n")
     entityListOld=entityListCollection.split("\n")
+
+    print("payloadOldList<")
+    payloadListOld=filterFieldsFromJSON(payloadListOld)
+
+    print(">\nentityOldList<")
+    entityListOld=filterFieldsFromJavaEntityFile(entityListOld)
+    print(">")
+
 
     payloadList={}
     entityList={}
@@ -56,16 +84,18 @@ text_Id
 
     notPresentfields=[]
     wrongCaseSensitiveFields=[]
+    print("Wrong Fields<")
     for field in payloadList.keys():
         if(field in entityList.keys()):
             if(payloadList[field]!=entityList[field]):
                 print(payloadList[field])
                 # wrongCaseSensitiveFields.append(payloadList[field)
         else: notPresentfields.append(field)
-    
-    print("\nField Not Present: ")
+    print(">")
+    print("\nField Not Present:< ")
     # print(notPresentfields)
     for field in notPresentfields:print(field)
+    print(">")
 
 
 popOutDifferentFields()
